5adc1f3e0c7581f87ea8ef37f4f8d2df11368e57
xuyi <08820048@users.noreply.github.com>
2026-04-03 20:22:48 +0800

    Normalize Homebrew cask R2 download source

--- Casks/ornata.rb
+++ Casks/ornata.rb
@@ -12,7 +12,7 @@ cask "ornata" do
          intel: "ed9506a25f79263924de85095dff796f093000e8310b523e258303f2deca7faf"
 
   url "https://download.ornata.app/updates/v1/darwin/#{folder_arch}/#{version}/Ornata_#{version}_#{dmg_arch}.dmg",
-      verified: "https://download.ornata.app/updates/v1/"
+      verified: "download.ornata.app/updates/v1/"
   name "Ornata"
   desc "Focused Markdown editor for writing and knowledge management"
   homepage "https://github.com/08820048/Ornata"
