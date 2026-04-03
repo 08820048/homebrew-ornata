d74372e6c3298eb2e00303abc1b9e3c942692685
xuyi <08820048@users.noreply.github.com>
2026-04-03 20:20:39 +0800

    Point cask downloads at public R2 CDN

--- Casks/ornata.rb
+++ Casks/ornata.rb
@@ -1,12 +1,18 @@
 cask "ornata" do
-  arch arm: "aarch64", intel: "x64"
+  if Hardware::CPU.arm?
+    dmg_arch = "aarch64"
+    folder_arch = "aarch64"
+  else
+    dmg_arch = "x64"
+    folder_arch = "x86_64"
+  end
 
   version "0.2.2"
   sha256 arm:   "f8c8d17e5cddfdf1b8a23ddd32f3ce98fd9cc7d24ae3b2f8b02a679b07561538",
          intel: "ed9506a25f79263924de85095dff796f093000e8310b523e258303f2deca7faf"
 
-  url "https://github.com/08820048/Ornata/releases/download/v#{version}/Ornata_#{version}_#{arch}.dmg",
-      verified: "github.com/08820048/Ornata/"
+  url "https://download.ornata.app/updates/v1/darwin/#{folder_arch}/#{version}/Ornata_#{version}_#{dmg_arch}.dmg",
+      verified: "https://download.ornata.app/updates/v1/"
   name "Ornata"
   desc "Focused Markdown editor for writing and knowledge management"
   homepage "https://github.com/08820048/Ornata"
